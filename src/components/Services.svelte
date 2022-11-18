424147003a45a7dae55abd83ef987ddae5bb029a
Brodie Peif <brodiep21@hotmail.com>
2022-11-18 07:24:42 -0500

    services fixed

--- src/components/Services.svelte
+++ src/components/Services.svelte
@@ -1,6 +1,6 @@
 <script>
     // import {fade} from 'svelte/transition'
-    import {devpic} from '../img/devpic.svg'
+    import devpic from '../img/devpic.svg'
 
     // const picture = [
     //     '../img/devpic.svg',
@@ -17,7 +17,9 @@
             <h3>Fit your needs</h3>
             <h1>My <br> Services.</h1>
         </div>
-        <div class="picture"><img src={devpic} alt="illustration of developer"></div>
+        <div class="picture">
+            <div class="picture2"><img src={devpic} alt="illustration of developer"></div>
+        </div>
         <div class="services2"></div>
     </div>
     
@@ -33,36 +35,74 @@
     background: var(--lorange);
     width: 100%;
     display: flex;
+    min-height:25rem;
 }
 .services1 h1 {
     font-size: 4rem;
     offset: left 5px;
 }
-.background {
-    width: 100%;
-
+.services2 {
+    width: 40%;
+    background-color: var(--red);
 }
 .picture {
-    width: 100px;
+    width: 70%;
+    /* overflow: hidden; */
+}
+.picture2 {
+    width: 40%;
+    margin-left: 75%;
+    padding-top: 1.6rem;
 }
 .picture img{
-    background-size: contain;
+    background-color: var(--white);
+    border-radius: 5rem;
+    box-shadow: 0px 0px 4px 0px rgba(0, 0, 0, 0.26);
+    height: 350px;
+    background-size: cover;
     background-repeat: no-repeat;
     transition: .3s ease-in-out;
-    height: 80px;
-    width: 80px;
+    position:relative;
+    z-index: 6;
+    
 }
+
 /* .picture:hover + .background{
     background-color: var(--black);
     width: 100px;
 } */
 
+/* <-----------------PHONE-------------> */
 @media screen and (max-width: 650px) {
     .services {
         padding-left: 5px;
     }
     .services h1 {
         font-size: 39px;
+    }
+    .picture {
+        /* text-align: left; */
+        padding-top: 1rem;
+    }
+    .picture2 {
+        margin-left: 0px;
+    }
+    .picture img {
+        height: 175px;
+    }
+}
+
+/* <---------------SMALLER SCREEN -----------> */
+@media screen and (min-width: 650px) and (max-width: 1050px) {
+    .picture {
+        /* text-align: left; */
+        padding-top: 1.6rem;
+    }
+    .picture2 {
+        margin-left: 40%;
+    }
+    .picture img {
+        height: 300px;
     }
 }
 </style>
